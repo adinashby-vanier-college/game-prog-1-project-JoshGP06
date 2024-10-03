9485e201c34dcc0ae628d8eedbec2585e16445a0
Joshua George Pangilinan <2475940@edu.vaniercollege.qc.ca>
2024-10-03 09:39:40 -0400

    updated

---
+++ Jeremiah's-Jungle/MyWorld.java
@@ -0,0 +1,32 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {
+        super(1705, 920, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Jeremiah jeremiah =  new Jeremiah();
+        addObject(jeremiah, 197, 795);
+        jeremiah.setLocation(74, 846);
+        jeremiah.setLocation(63, 853);
+    }
+}
