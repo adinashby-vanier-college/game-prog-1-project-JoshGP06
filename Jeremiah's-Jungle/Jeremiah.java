2cf1eddd5e3363b277498cc0ef3a4fa10a471330
Joshua George Pangilinan <2475940@edu.vaniercollege.qc.ca>
2024-10-22 09:35:24 -0400

    added movement, please add the ground classes and whatnot

--- Jeremiah's-Jungle/Jeremiah.java
+++ Jeremiah's-Jungle/Jeremiah.java
@@ -8,11 +8,55 @@ import greenfoot.*;
  */
 public class Jeremiah extends Actor
 {
+    private int jumpHeight = 10;
+    private int gravity = 1;
+    private int verticalSpeed = 0;
+    private boolean onGround = true;
+    
 
     /**
      * Act - do whatever the Jeremiah wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+        movement();
+        jump();
+        moveVertically();
+    }
+
+    /**
+     * 
+     */
+    public void movement()
+    {
+        if (Greenfoot.isKeyDown("d")) {
+            move(4);
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            move(-3);
+            
+        }
+    }
+
+    /**
+     * 
+     */
+    public void jump()
+    {
+        if (Greenfoot.isKeyDown("space") && onGround) {
+            verticalSpeed =  - jumpHeight;
+            onGround = false;
+        }
+        if ( ! onGround) {
+            verticalSpeed = gravity;
+        }
+    }
+
+    /**
+     * 
+     */
+    private void moveVertically()
+    {
+        setLocation(getX(), getY() + verticalSpeed);
     }
 }
